c0a77496ec69e540bcebaaca51fb6a2b547fa540
TysonHentegs-12 <821583@mystma.org>
2022-09-16 14:15:36 -0500

    2

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 #WRITE YOUR CODE IN THIS FILE
 def topFiveMovies(a, b, c, d, e):
     return a + b + c + d + e
-print(topFiveMovies("1. Deadpool", " 2. Free Guy", " 3. Jumanji", " 4. Jumanji: Welcome to the jungle", " 5. Deadpool 2"))
