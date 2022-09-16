c2d0781a568d2311053afc0f820ebba7321b7ad5
TysonHentegs-12 <821583@mystma.org>
2022-09-16 14:20:08 -0500

    3

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 #WRITE YOUR CODE IN THIS FILE
-def topFiveMovies(a, b, c, d, e):
+def topFiveMovies(a, b, c, d, e ):
     return a + b + c + d + e
+print(topFiveMovies("a ", "b ", "c ", "d ", "e "))
